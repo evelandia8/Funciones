079b8cb03d2d142f4ede48152f8983ded41c226a
Leonardo Velandia <evelandia8@estudiantes.areandina.edu.co>
2022-06-29 21:57:18 -0500

    Diccionarios.py

---
+++ diccionarios.py
@@ -0,0 +1,12 @@
+from cgi import print_form
+
+
+Paises={"Colombia":"Bogotá","Peru":"Lima","lista":[1,2,3,4,5]}
+
+print(Paises)
+
+print(Paises["lista"][1])
+
+print(Paises.keys())
+
+print(Paises.values())
